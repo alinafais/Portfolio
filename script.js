85236db907348914344947e4c40f8ae2fa311f89
Alina Faisal <alinafaisal151@gmail.com>
2025-01-10 03:30:36 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".text", {
-    strings: ["Product Designer . . .", "Frontend Web Developer . . ." , "UI/UX Designer . . ." , "Machine Learning and AI Engineer . . .", "UX Researcher . . .", "Data Scientist . . ."],
+    strings: ["Product Designer . . .", "Frontend Web Developer . . ." , "UI/UX Designer . . ." , "UX Researcher . . .", "Software Developer . . .", "UX Writer . . .", "UX Engineer . . .",],
     typeSpeed: 70,
     backSpeed: 50,
     backDelay: 1000,
